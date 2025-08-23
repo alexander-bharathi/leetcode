adcfca7ff4bc640c7b4c370c2c9e30b1cbb49f65
alexander-bharathi <alexanderbdeveloper@gmail.com>
2025-08-23 20:26:11 +0530

    javascript 1588_sum_of_all_odd_length_subarrays(23/08/25)

---
+++ 1588_sum_of_all_odd_length_subarrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function (arr) {
+  let n = arr.length;
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+    let left = i + 1;
+    let right = n - i;
+    let totalSubarrays = left * right;
+    let oddCount = Math.floor((totalSubarrays + 1) / 2);
+    total += arr[i] * oddCount;
+  }
+  return total;
+};
