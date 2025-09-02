0ab26176f587f8a79e76f345b8eb2872940d4678
alexander-bharathi <alexanderbdeveloper@gmail.com>
2025-09-02 21:50:10 +0530

    sample commit

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -20,3 +20,7 @@ var lengthOfLongestSubstring = function (s) {
 // Input: s = "abcabcbb"
 // Output: 3
 // Explanation: The answer is "abc", with the length of 3.
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
