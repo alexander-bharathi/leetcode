6d188c0c89b6f11a91a6c947851995157bbcd4e8
alexander-bharathi <alexanderbdeveloper@gmail.com>
2025-09-17 22:32:16 +0530

    javascript 3191_minimum_operations_to_make_binary_array_elements_equal_to_one_I(17/09/25)

---
+++ 3191_minimum_operations_to_make_binary_array_elements_equal_to_one_I.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function (nums) {
+  let n = nums.length;
+  let ops = 0;
+  if (n < 3) return nums.every(x => x === 1) ? 0 : -1;
+  for (let i = 0; i <= n - 3; i++) {
+    if (nums[i] === 0) {
+      nums[i] ^= 1;
+      nums[i + 1] ^= 1;
+      nums[i + 2] ^= 1;
+      ops++;
+    }
+  }
+  return (nums[n - 2] === 0 || nums[n - 1] === 0) ? -1 : ops;
+};
+// Input: nums = [0, 1, 1, 1, 0, 0]
+// Output: 3
+// Explanation:
+// We can do the following operations:
+// Choose the elements at indices 0, 1 and 2. The resulting array is nums = [1, 0, 0, 1, 0, 0].
+// Choose the elements at indices 1, 2 and 3. The resulting array is nums = [1, 1, 1, 0, 0, 0].
+// Choose the elements at indices 3, 4 and 5. The resulting array is nums = [1, 1, 1, 1, 1, 1].
