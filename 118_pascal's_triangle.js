11eae4336b38f650b34d701bbd92348c80926641
alexander-bharathi <alexanderbdeveloper@gmail.com>
2025-10-15 18:39:19 +0530

    javascript 118_pascal's_triangle(15/10/25)

---
+++ 118_pascal's_triangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  if (numRows === 0) return [];
+  let result = [[1]];
+  for (let i = 1; i < numRows; i++) {
+    const rows = [];
+    const previousRow = result[i - 1];
+    for (let j = 0; j <= previousRow.length; j++) {
+      if (j == 0 || j == previousRow.length) rows.push(1);
+      else rows.push(previousRow[j] + previousRow[j - 1]);
+    }
+    result.push(rows);
+  }
+  return result;
+};
