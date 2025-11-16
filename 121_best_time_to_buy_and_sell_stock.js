a82ead676b0fc1ced0201c83be982ce3af7ad2c7
alexander-bharathi <alexanderbdeveloper@gmail.com>
2025-11-16 16:49:57 +0530

    javascript 121_best_time_to_buy_and_sell_stock(16/11/25)

---
+++ 121_best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+  let buyPrice = prices[0];
+  let max = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < buyPrice) {
+      buyPrice = prices[i]
+    } else {
+      profit = prices[i] - buyPrice;
+      max = Math.max(max, profit);
+    }
+  }
+  return max;
+};
+
+// Input: prices = [7, 1, 5, 3, 6, 4]
+// Output: 5
+// Explanation: Buy on day 2(price = 1) and sell on day 5(price = 6), profit = 6 - 1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
