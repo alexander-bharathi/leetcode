0f4e055fa588cd6e2faee3255c4f85ac760aeece
Alexander Bharathi <alexanderbdevelopergmail.com@Alexanders-MacBook-Air.local>
2026-01-01 18:19:03 +0530

    javascript 347_top_K_frequent_elements(01/01/26)

---
+++ 347_top_K_frequent_elements.js
@@ -0,0 +1,10 @@
+var topKFrequent = function (nums, k) {
+    const map = new Map();
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    return [...map.entries()]
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, k)
+        .map(entry => entry[0]);
+};
