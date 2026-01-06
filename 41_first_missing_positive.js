fd95017bafb40f66f995a76797bd429ce3b6eb01
alexander-bharathi <alexanderbdeveloper@gmail.com>
2026-01-06 22:06:19 +0530

    javascript 41_first_missing_positive(06/01/26)

---
+++ 41_first_missing_positive.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+    const set = new Set();
+    for (let num of nums) {
+        if (num > 0) {
+            set.add(num);
+        }
+    }
+    let i = 1;
+    while (true) {
+        if (!set.has(i)) return i;
+        i++;
+    }
+};
