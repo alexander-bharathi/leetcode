dd903d93e37bca726c7eb2ad7ef6185b5978d744
alexander-bharathi <alexanderbdeveloper@gmail.com>
2026-01-08 22:46:49 +0530

    javascript 496_next_greater_element_I(08/01/26)

---
+++ 496_next_greater_element_I.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+    const stack = [];
+    const nextGreater = new Map();
+    for (let num of nums2) {
+        while (stack.length && num > stack[stack.length - 1]) {
+            console.log(stack,'d');
+            nextGreater.set(stack.pop(), num);
+        }
+        stack.push(num);
+    }
+    while (stack.length) {
+        nextGreater.set(stack.pop(), -1);
+    }
+    return nums1.map(num => nextGreater.get(num));
+};
