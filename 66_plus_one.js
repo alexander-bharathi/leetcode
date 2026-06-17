2783854a116f4580c5ac59316dd35572b53a72aa
alexander-bharathi <alexanderbdeveloper@gmail.com>
2026-06-17 20:35:38 +0530

    changes

--- 66_plus_one.js
+++ 66_plus_one.js
@@ -4,10 +4,7 @@ var plusOne = function (digits) {
         digits[id] = 0;
         id--;
     }
-    if (id < 0) {
-        digits.unshift(1);
-    } else {
-        digits[id]++;
-    }
+    if (id < 0) digits.unshift(1);
+    else digits[id]++;
     return digits;
 };
